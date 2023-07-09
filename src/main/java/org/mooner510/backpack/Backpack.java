3569d1c893fe15adb8f9f0ef2eb3f020feb39d08
Mooner510 <lmck510@gmail.com>
2023-07-09 23:04:45 +0900

    Initial commit

---
+++ src/main/java/org/mooner510/backpack/Backpack.java
@@ -0,0 +1,17 @@
+package org.mooner510.backpack;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Backpack extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
